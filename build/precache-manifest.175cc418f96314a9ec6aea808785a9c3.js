a20fd9cebc5c6ad6372e797bb03caedae6cc2987
aurelie hautbois <aurelie.hautbois@gmail.com>
2020-07-05 12:56:56 +0200

    add build folder

---
+++ build/precache-manifest.175cc418f96314a9ec6aea808785a9c3.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aceeef3e73b001331545778f62b3cfe2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "95027ee3e5baddd47a5f",
+    "url": "/static/css/main.05c307e5.chunk.css"
+  },
+  {
+    "revision": "2432348d33cfd7e79d2e",
+    "url": "/static/js/2.688e58fc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.688e58fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95027ee3e5baddd47a5f",
+    "url": "/static/js/main.bacf1776.chunk.js"
+  },
+  {
+    "revision": "ea50cf8fc26d624d3317",
+    "url": "/static/js/runtime-main.56b65086.js"
+  },
+  {
+    "revision": "aced70522265bc609155bea44802776b",
+    "url": "/static/media/bg-pentagon.aced7052.svg"
+  },
+  {
+    "revision": "7d2e0b18d2860b90407f41297dce461f",
+    "url": "/static/media/icon-lizard.7d2e0b18.svg"
+  },
+  {
+    "revision": "69a8c2e96b659b35a3760c79123be610",
+    "url": "/static/media/icon-paper.69a8c2e9.svg"
+  },
+  {
+    "revision": "8ce1e109bb43912192f5ba8dfcca005e",
+    "url": "/static/media/icon-rock.8ce1e109.svg"
+  },
+  {
+    "revision": "bec2f9b3d60cb80987106bb49205c53f",
+    "url": "/static/media/icon-scissors.bec2f9b3.svg"
+  },
+  {
+    "revision": "b6446e8e1e5d5609deb209d401e779a3",
+    "url": "/static/media/icon-spock.b6446e8e.svg"
+  },
+  {
+    "revision": "a5ea827bd56f5d5b9d0daf08a4646188",
+    "url": "/static/media/image-rules-bonus-opti.a5ea827b.svg"
+  }
+]);
